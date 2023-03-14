b51a158492f876a00f93a3c4b279b239341c4e89
DavidJSolano <dsolano7@csu.fullerton.edu>
2023-03-13 21:21:04 -0700

    Fixed forms

--- my-svelte-project/src/components/App.svelte
+++ my-svelte-project/src/components/App.svelte
@@ -77,136 +77,195 @@
   .featured ul {
     list-style: none;
   }
-  body {
-        font-family: Arial, sans-serif;
-        margin: 0;
-        padding: 0;
-      }
+  * {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+/* Body styles */
+body {
+  font-family: Arial, sans-serif;
+  font-size: 16px;
+  line-height: 1.5;
+  background-color: #f2f2f2;
+}
+
+/* Main styles */
+main {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+/* Header styles */
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+h1 {
+  font-size: 36px;
+  font-weight: bold;
+  color: #333;
+}
+
+nav ul {
+  list-style: none;
+  display: flex;
+}
+
+nav li {
+  margin-left: 20px;
+}
+
+nav a {
+  text-decoration: none;
+  color: #333;
+  font-weight: bold;
+}
+
+/* Featured section styles */
+.featured {
+  margin-bottom: 40px;
+}
+
+.featured h2 {
+  font-size: 24px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 20px;
+}
+
+ul {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 3px 10px rgba(0, 0, 0, 0.1);
+}
+
+img {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-bottom: 20px;
+}
+
+h3 {
+  font-size: 20px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 10px;
+}
+
+p {
+  font-size: 16px;
+  color: #777;
+  margin-bottom: 10px;
+}
+
+.btn {
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #333;
+  color: #fff;
+  text-decoration: none;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+}
+
+.btn:hover {
+  background-color: #555;
+}
+
+/* Search section styles */
+.search h2 {
+  font-size: 24px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 20px;
+}
+
+form {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 3px 10px rgba(0, 0, 0, 0.1);
+}
       
-      /* Header styles */
-      header {
-        background-color: #00bcd4;
-        color: #fff;
-        display: flex;
-        justify-content: space-between;
-        padding: 20px;
-      }
-      
-      h1 {
-        margin: 0;
-      }
-      
-      nav ul {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        display: flex;
-      }
-      
-      nav li {
-        margin: 0 10px;
-      }
-      
-      nav li a {
-        color: #fff;
-        text-decoration: none;
-        font-weight: bold;
-      }
-      
-      /* Featured section styles */
-      .featured {
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-        flex-wrap: wrap;
-        padding: 50px 0;
-      }
-      
-      .featured li {
-        background-color: #f2f2f2;
-        margin: 20px;
-        padding: 20px;
-        text-align: center;
-        border-radius: 5px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-      }
-      
-      .featured li img {
-        border-radius: 50%;
-        margin-bottom: 20px;
-      }
-      
-      .featured li h3 {
-        margin: 0;
-      }
-      
-      .featured li p {
-        margin: 0;
-        font-size: 14px;
-        color: #888;
-      }
-      
-      .featured li .btn {
-        display: block;
-        margin-top: 20px;
-        background-color: #00bcd4;
-        color: #fff;
-        padding: 10px 20px;
-        text-align: center;
-        text-decoration: none;
-        border-radius: 5px;
-        transition: background-color 0.3s ease;
-      }
-      
-      .featured li .btn:hover {
-        background-color: #007a8a;
-      }
-      
-      /* Search section styles */
-      .search {
-        background-color: #eee;
-        padding: 50px;
-        text-align: center;
-      }
-      
-      .search h2 {
-        margin-top: 0;
-      }
-      
-      .search form {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-        margin-top: 20px;
-      }
-      
-      .search label {
-        font-weight: bold;
-        margin-right: 10px;
-      }
-      
-      .search input[type="text"],
-      .search select {
-        padding: 10px;
-        border-radius: 5px;
-        border: none;
-        margin-right: 10px;
-        margin-bottom: 10px;
-      }
-      
-      .search button {
-        background-color: #00bcd4;
-        color: #fff;
-        border: none;
-        border-radius: 5px;
-        padding: 10px 20px;
-        text-align: center;
-        text-decoration: none;
-        transition: background-color 0.3s ease;
-      }
-      
-      .search button:hover {
-        background-color: #007a8a;
-      }
+.featured ul li img {
+  display: block;
+  margin: 0 auto;
+}
+
+/* Styling for the search section */
+.search {
+  text-align: center;
+}
+
+.search h2 {
+  font-size: 2rem;
+  margin-top: 40px;
+}
+
+.search form {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.search label {
+  font-weight: bold;
+  margin-right: 10px;
+  display: inline-block;
+}
+
+.search input[type="text"],
+.search select {
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  margin-right: 5px;
+  margin-bottom: 0px;
+  width: 200px;
+}
+
+.search button {
+  background-color: #00bcd4;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  transition: background-color 0.3s ease;
+  display: inline-block;
+}
+
+.search button:hover {
+  background-color: #007a8a;
+}
 
 </style>
